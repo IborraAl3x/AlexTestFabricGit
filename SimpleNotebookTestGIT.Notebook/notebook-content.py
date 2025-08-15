a1b4b73ec612d59ce8f015a8fa02ca73bb94b1a3
IborraAl3x <aiborra@goodwinlaw.com>
2025-08-15 13:15:37 -0400

    Committing 1 item from workspace 2a43903a-039a-41ea-9439-d841505c55e5

--- SimpleNotebookTestGIT.Notebook/notebook-content.py
+++ SimpleNotebookTestGIT.Notebook/notebook-content.py
@@ -6,12 +6,35 @@
 # META   "kernel_info": {
 # META     "name": "synapse_pyspark"
 # META   },
-# META   "dependencies": {}
+# META   "dependencies": {
+# META     "lakehouse": {
+# META       "default_lakehouse": "01babc29-5ad2-4646-b1c8-febbefe48e40",
+# META       "default_lakehouse_name": "DS_MONITOR_LH",
+# META       "default_lakehouse_workspace_id": "cba393b2-3ed0-42fb-bd14-eea430b01e7b",
+# META       "known_lakehouses": [
+# META         {
+# META           "id": "01babc29-5ad2-4646-b1c8-febbefe48e40"
+# META         }
+# META       ]
+# META     }
+# META   }
 # META }
 
 # CELL ********************
 
 print("hello world")
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+df = spark.sql("SELECT * FROM DS_MONITOR_LH.dim_date LIMIT 1000")
+display(df)
 
 # METADATA ********************
 
